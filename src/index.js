c8376811db8aad6361f481c8a9d226932f5580d4
Magdalena Uszynska <magdalenauszynska1@gmail.com>
2023-12-08 20:22:09 +0100

    change css animation

--- src/index.js
+++ src/index.js
@@ -12,12 +12,14 @@ const error = document.querySelector('.error');
 
 const ref = { selector, catInfo, loader, error };
 
-loader.classList.replace('loader', 'is-hidden');
-error.classList.add('is-hidden');
+// loader.classList.replace('loader', 'is-hidden');
+// error.classList.add('is-hidden');
 catInfo.classList.add('is-hidden');
+
 
 let arrIdBreeds = [];
 fetchBreeds()
+    
     .then(data => {
         data.forEach(element => {
             arrIdBreeds.push({ text: element.name, value: element.id });
@@ -27,8 +29,11 @@ fetchBreeds()
             select: selector,
             data: arrIdBreeds,
         });
+
     })
-    .catch(onErrorFetch);
+    .catch(onErrorFetch)
+    // .finally(hiddenLoader);
+
 selector.addEventListener('change', onBreedSelect);
 
 function onErrorFetch(error) {
@@ -41,9 +46,14 @@ function onErrorFetch(error) {
         fontSize: '24px'
     });
 };
+// function hiddenLoader(a) {
+//     // selector.classList.remove('loader');
+//     loader.classList.replace('is-hidden', 'loader');
+// }
 
 function onBreedSelect(event) {
     loader.classList.replace('is-hidden', 'loader');
+    
     selector.classList.add('is-hidden');
     catInfo.classList.add('is-hidden');
 
@@ -51,11 +61,15 @@ function onBreedSelect(event) {
     fetchCatByBreed(idBreed)
         .then(data => {
             loader.classList.replace('loader', 'is-hidden');
+            
             selector.classList.remove('is-hidden');
             const { url, breeds } = data[0];
             catInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
             catInfo.classList.remove('is-hidden');
         })
         .catch(onErrorFetch);
+        
 
 };
+
+
